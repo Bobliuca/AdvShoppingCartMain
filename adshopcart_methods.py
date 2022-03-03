e1bbfecd5b52de809490821dffb8190408115176
Bobliuca <boaleo.ca@gmail.com>
2022-03-03 09:49:30 -0800

    Lab 4 united

--- adshopcart_methods.py
+++ adshopcart_methods.py
@@ -10,8 +10,8 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(r':\Users\asus\PycharmProjects\python-cctb\chromedriver.exe')
 
-s = Service(executable_path='./chromedriver')
-driver = webdriver.Chrome(service=s)
+# s = Service(executable_path='./chromedriver')
+# driver = webdriver.Chrome(service=s)
 
 def setUp():
 
@@ -165,7 +165,57 @@ def check_user_deleted():
             print('Test Scenario: Check the new user deleted --- is passed!')
 
     # breakpoint()
+def check_homepage():
+    if driver.find_element(By.ID, 'speakersTxt') \
+            and driver.find_element(By.ID, 'tabletsTxt') \
+            and driver.find_element(By.ID, 'laptopsTxt') \
+            and driver.find_element(By.ID, 'miceTxt') \
+            and driver.find_element(By.ID, 'headphonesTxt'):
+        print('_______SPEAKERS, TABLETS, HEADPHONES, LAPTOPS, MICE___________are displayed')
+    else:
+        print('_______SPEAKERS, TABLETS, HEADPHONES, LAPTOPS, MICE___________are not displayed')
+    sleep(.25)
+    driver.find_element(By.LINK_TEXT, 'SPECIAL OFFER').click()
+    sleep(.25)
+    if driver.find_element(By.ID, 'special_offer_items').is_displayed():
+        print('"SPECIAL OFFER" button is clickable!')
+    else:
+        print('SPECIAL OFFER button is not clickable!')
+    driver.find_element(By.LINK_TEXT, 'POPULAR ITEMS').click()
+    sleep(.25)
+    if driver.find_element(By.ID, 'popular_items').is_displayed():
+        print('"POPULAR ITEMS" button is clickable!')
+    else:
+        print('"POPULAR ITEMS" button is not clickable!')
+    driver.find_element(By.LINK_TEXT, 'CONTACT US').click()
+    sleep(.25)
+    if driver.find_element(By.ID, 'contact_us').is_displayed():
+        print('"CONTACT US" button is clickable!')
+    else:
+        print('CONTACT US button is not clickable!')
+    sleep(.25)
+    if driver.find_element(By.XPATH, '//span[contains(., "dvantage")]').is_displayed():
+        print('The dvantageDEMO Logo is displayed!')
+    else:
+        print('Logo error')
 
+def fill_contact_infor():
+    driver.find_element(By.ID, 'contact_us').click()
+    Select(driver.find_element(By.NAME, 'categoryListboxContactUs')).select_by_visible_text('Laptops')
+    sleep(.25)
+    assert driver.find_element(By.XPATH, '//*[@id="supportCover"]/div[2]/sec-form/div[1]/div/sec-view[1]\
+    /div/select/option[2]').is_displayed()
+    driver.find_element(By.XPATH, '//*[@id="supportCover"]\
+    /div[2]/sec-form/div[1]/div/sec-view[3]/div/input').send_keys(locators.email)
+    sleep(.5)
+    driver.find_element(By.NAME, 'subjectTextareaContactUs').send_keys(locators.description)
+    sleep(.5)
+    driver.find_element(By.ID, 'send_btnundefined').click()
+    sleep(.25)
+    if driver.find_element(By.ID, 'registerSuccessCover').is_displayed():
+        print('------------------Contact Us function test passed!---------------')
+    else:
+        print('!!!!!!!!!!!!Contact Us function test failed!')
 
 
 
--- adshopcart_tests.py
+++ adshopcart_tests.py
@@ -8,6 +8,8 @@ class AdShopCartPositiveTestCase(unittest.TestCase):
     @staticmethod
     def test_adshopcart():
         methods.setUp()
+        methods.check_homepage()
+        methods.fill_contact_infor()
         methods.create_new_user()
         methods.log_out()
         methods.log_in()
